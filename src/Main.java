b83ab72b9e147579c9c1fbdc586f7df748620d41
MrJerry <my_main.com>
2025-04-02 16:28:43 +0300

    removed all classes and created a new

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,3 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Hello!");
-        System.out.println("Hello!");
-        System.out.println("Hello!");
 
-        System.out.println("I sad let its gone!");
-    }
 }
